1906d303e19b12caa178ca5ca53ec76a30c92f8e
Andres <andresomardaglio@gmail.com>
2026-05-29 16:17:02 -0300

    agrega consulta para traer caracteristicas de las canchas al filtro de clubes

--- models/club.model.js
+++ models/club.model.js
@@ -8,16 +8,25 @@ const getAllClubs = async () => {
       c.direccion,
       c.telefono,
       c.imagen_url,
-      GROUP_CONCAT(car.nombre SEPARATOR ',') AS caracteristicas
+      GROUP_CONCAT(DISTINCT car_club.nombre ORDER BY car_club.nombre SEPARATOR ',') AS caracteristicas_club,
+      GROUP_CONCAT(DISTINCT car_cancha.nombre ORDER BY car_cancha.nombre SEPARATOR ',') AS caracteristicas_canchas
     FROM clubes c
     LEFT JOIN club_caracteristica cc ON c.club_id = cc.club_id
-    LEFT JOIN caracteristicas car ON cc.caract_id = car.caract_id
+    LEFT JOIN caracteristicas car_club ON cc.caract_id = car_club.caract_id
+    LEFT JOIN canchas ca ON ca.club_id = c.club_id
+    LEFT JOIN cancha_caracteristica cca ON cca.cancha_id = ca.cancha_id
+    LEFT JOIN caracteristicas car_cancha ON cca.caract_id = car_cancha.caract_id
     GROUP BY c.club_id
   `);
 
   return clubs.map((club) => ({
     ...club,
-    caracteristicas: club.caracteristicas ? club.caracteristicas.split(",") : [],
+    caracteristicas: club.caracteristicas_club
+      ? club.caracteristicas_club.split(',')
+      : [],
+    caracteristicas_canchas: club.caracteristicas_canchas
+      ? [...new Set(club.caracteristicas_canchas.split(','))]
+      : [],
   }));
 };
 
