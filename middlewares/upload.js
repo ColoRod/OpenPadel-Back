02c03946834dea13d8ca46782ae0bf305eddeac2
Thomas Cardarelli <128648457+ThomiCardsrelli@users.noreply.github.com>
2026-08-14 15:11:11 -0300

    Update upload.js

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,18 +1,6 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-
-  filename: function (req, file, cb) {
-    const uniqueName =
-      Date.now() + "-" + Math.round(Math.random() * 1e9) +
-      "-" + file.originalname;
-
-    cb(null, uniqueName);
-  },
-});
+const storage = multer.memoryStorage();
 
 export const upload = multer({
   storage,
